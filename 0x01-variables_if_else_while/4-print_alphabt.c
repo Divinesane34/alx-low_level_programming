aa69e6b12337ce44ae172717de2e2e55296a61df
Divinesane34 <dboynwabueze48@gmail.com>
2022-10-17 13:16:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char alp[24] = "abcdfghijklmnoprstuwxyz";
+
+	for (i = 0; i < 24; i++)
+	{
+		printf("%c", alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
